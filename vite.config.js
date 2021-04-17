493353f235059a70c24aa74604fd4132e90a773f
Georg Hartmann <mail@georg-hartmann.com>
2021-04-17 10:53:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-// “Why programmers like UNIX: unzip, strip, touch, finger, grep, mount, fsck, more, yes, fsck, fsck, fsck, umount, sleep.”
 export default defineConfig({
   plugins: [svelte()]
 })
